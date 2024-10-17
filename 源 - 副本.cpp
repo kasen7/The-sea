9ee77d04f9fa2e8095ff14c8dfd6a62799abf965
kasen7 <148206030+kasen7@users.noreply.github.com>
2024-10-17 18:22:32 +0800

    Add files via upload

---
+++ 源 - 副本.cpp
@@ -0,0 +1,104 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//void test1()
+//{
+//	string s1;
+//	s1 = "aaa";
+//	cout << s1 << endl;
+//
+//	string s2("bbbb");
+//	cout << s2 << endl;
+//
+//	string s3=s2;
+//	cout << s3 << endl;
+//
+//	string s4(s3);
+//	cout << s4 << endl;
+//
+//	string s5;
+//	char s[] = { "cccc" };
+//	s5 = s;
+//	cout << s5 << endl;
+//
+//	string s6(s);
+//	cout << s6 << endl;
+//
+//	string s7("hello world");
+//	string s8(3,'d');
+//	cout << s7 << endl << s8 << endl;
+//
+//	string s9(s7,0,5);
+//	cout << s9;
+//}
+//
+//void test2()
+//{
+//	string s = "abcde";
+//	cout << s.size() << ' ' << s.length() << endl;
+//
+//	cout << s.capacity() << endl;
+//
+//	s.reserve(50);
+//	cout << s.capacity() << endl;
+//	s += "hijklmnopqargawgrawrg";
+//	cout << s.capacity() << endl;
+//
+//	s.resize(100,'q');
+//	cout << s << endl;
+//
+//	string s2;
+//	s2.resize(5, 'a');
+//	cout << s2 << endl;
+//
+//	if (!s.empty())cout << "NO" << endl;
+//
+//	s.clear();
+//	if (s.empty())cout << "YES" << endl;
+//
+//
+//}
+/*
+void test3()
+{
+	string str("123456");
+	cout << str[0] << endl;
+
+	char begin = str[0];
+	cout << begin << endl;
+	cout << *str.begin() << endl;
+	cout << *(str.end()-3) << endl;
+
+	for (auto a: str)
+	{
+		cout << a << ' ';
+	}
+
+	cout << endl<<strlen(str.c_str()) << endl;
+
+}
+*/
+
+//void test4()
+//{
+//	string s1("ab");
+//	string sr("abc,de,f,g");
+//	size_t s=sr.find(',');
+//
+//	size_t s2 = sr.rfind(',');
+//	//cout << string::npos << endl;
+//	cout << sr[s] <<' '<<s<< endl;
+//
+//	string s3 = sr.substr(1,3);
+//	cout << s3;
+//}
+
+int main()
+{
+	//test1();
+	//test3();
+
+	//test4();
+	return 0;
+}
